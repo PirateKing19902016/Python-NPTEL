8f3a5ba147c739cea4cf68c4447aca2cef06d00f
PirateKing Windows <nikhilsharma19902016@gmail.com>
2018-03-01 20:04:24 +0530

    Binary Search

--- Temporary COding File.py
+++ Temporary COding File.py
@@ -1,14 +1,22 @@
-# Euclid's algorithm
+a = [[1, 2], [3,4]]
+b = [[1, 0], [0,1]]
 
+zip_b = list(zip(*b))
 
-def gcd(m, n):
-    if m < n:
-        m, n = n, m
-    if m % n == 0:
-        return n
-    else:
-        diff = m - n
-        return gcd(n, diff)
+sum_of_product_of_element, product_of_element = 0, 0
+matrix = []
+tempRow = []
+for row_a in a:
+    for col_b in zip_b:
+        x = 0
+        for ele_a, ele_b in zip(row_a, col_b):
 
-
-print("GCD of 10 and 20 is",gcd(10, 20))
+            product_of_element = ele_a * ele_b
+            sum_of_product_of_element += product_of_element
+        # print(sum_of_product_of_element)
+        tempRow.append(sum_of_product_of_element)
+        sum_of_product_of_element=0
+    # print(tempRow)
+    matrix.append(tempRow)
+    tempRow = []
+print(matrix)
